45af4fe2fc53a43976d886250bdd8b60c43a34bb
Sam De Maeyer <sam_demaeyer@hotmail.com>
2021-03-13 16:44:00 +0000

    Add all planets

--- devil/index.js
+++ devil/index.js
@@ -1,3 +1,20 @@
+const urlParams = new URLSearchParams(window.location.search)
+const hasImgParam = urlParams.get('has-image')
+const params = [
+  [ 'niac', urlParams.get('niac')],
+  [ 'zurich', urlParams.get('zurich')],
+  [ 'smart', urlParams.get('smart')],
+  [ 'nemo', urlParams.get('nemo')],
+  [ 'ci', urlParams.get('ci')],
+].filter(i => i[1])
+
+params.forEach(([planet, param]) => {
+  document.querySelector(`.${planet}`)?.classList.add(param)
+})
+
+params.some(([_, param]) => param === 'fail') && document.body.classList.add('fail-bg')
+hasImgParam && document.body.classList.add('has-img')
+
 const stopAudio = (audio) => {
   audio.pause()
   audio.currentTime = 0
@@ -21,9 +38,8 @@ document.getElementById('stop-sound').addEventListener('click', () => {
   sounds.forEach(stopAudio)
 })
 
-document.querySelector('.wrapper').addEventListener('click', () => {
+document.querySelector('.ci').addEventListener('click', () => {
   sounds.forEach(stopAudio)
-  const urlParams = new URLSearchParams(window.location.search)
   const param = urlParams.get('sound') || undefined
   const audio = sounds[+param < sounds.length ? +param : soundIndex]
   audio.play()
